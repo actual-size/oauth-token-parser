5ef1e7cc057b443e2e0d26c162ee4c90520132c4
River Smith <22485304+actual-size@users.noreply.github.com>
2020-07-07 14:21:07 +1000

    Remove boilerplate from markup

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,7 @@
 <script>
   import { onMount } from "svelte";
-  export let date;
   let token;
 
-  onMount(async () => {
-    const res = await fetch("/api/date");
-    const newDate = await res.text();
-    date = newDate;
-  });
   onMount(async () => {
       let query = window.location.search.substring(0);
       token = query.split("&").map((param) => {
@@ -20,18 +14,6 @@
 </script>
 
 <main>
-  <h1>Svelte + Node.js API</h1>
-  <h2>
-    Deployed with
-    <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">
-      Vercel
-    </a>
-    !
-  </h2>
-  <p>
-    <h1>{token}</h1>
-  </p>
-  <br />
-  <h2>The date according to Node.js is:</h2>
-  <p>{date ? date : 'Loading date...'}</p>
+  <h1>Your token is:</h1>
+  <h2>{token}</h2>
 </main>
